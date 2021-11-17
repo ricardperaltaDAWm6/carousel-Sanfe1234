1747a828a3687d64cbb335000dfbfed77352fcb0
Sanfe02 <18401397+Sanfe1234@users.noreply.github.com>
2021-11-17 15:45:15 +0100

    canvis afegits

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     // Variables
 
     // Añadir las tres imágenes del directorio "img" al array IMAGENES.
-    const IMAGENES = [];
+    const IMAGENES = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
 
@@ -10,14 +10,17 @@ window.onload = function () {
     let posicionActual = 0;
 
     // variables con los elementos del DOM HTML, aplicar el selector necesario.
-    let $botonRetroceder
-    let $botonAvanzar 
-    let $imagen 
-    let $botonPlay 
-    let $botonStop
+    let $botonRetroceder = document.getElementById("retroceder");
+    let $botonAvanzar = document.getElementById("avanzar");
+    let $imagen = document.getElementById("imagen");
+    let $botonPlay = document.getElementById("play");
+    let $botonStop = document.getElementById("stop");
+    let $imgName = document.getElementById("name");
 
     // Identificador del proceso que se ejecuta con setInterval().
     let intervalo;
+
+    let playrunning = false;
 
     // Funciones
 
@@ -26,8 +29,12 @@ window.onload = function () {
      */
     function pasarFoto() {
         // se incrementa el indice (posicionActual)
-
+        posicionActual++;
+        if (posicionActual >= IMAGENES.length) {
+            posicionActual = 0;
+        }
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
@@ -35,8 +42,12 @@ window.onload = function () {
      */
     function retrocederFoto() {
         // se incrementa el indice (posicionActual)
-
+        posicionActual--;
+        if (posicionActual < 0) {
+            posicionActual = IMAGENES.length - 1;
+        }
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
@@ -52,10 +63,13 @@ window.onload = function () {
     function playIntervalo() {
         // Documentación de la función setInterval: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
         // Mediante la función setInterval() se ejecuta la función pasarFoto cada TIEMPO_INTERVALO_MILESIMAS_SEG.
-        
-
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG)
         // Desactivamos los botones de control necesarios. Utilizando setAttribute y removeAttribute.
-
+        $botonPlay.setAttribute("disabled", "")
+        $botonRetroceder.setAttribute("disabled", "")
+        $botonAvanzar.setAttribute("disabled", "")
+        $botonStop.removeAttribute("disabled", "");
+        playrunning = true;
     }
 
     /**
@@ -63,13 +77,62 @@ window.onload = function () {
      */
     function stopIntervalo() {
         // Desactivar la ejecución de intervalo.
-
+        clearInterval(intervalo);
+        intervalo = null;
         // Activamos los botones de control. Utilizando setAttribute y removeAttribute.
+        $botonStop.setAttribute("disabled", "")
+        $botonAvanzar.removeAttribute("disabled", "")
+        $botonRetroceder.removeAttribute("disabled", "")
+        $botonPlay.removeAttribute("disabled", "")
+        playrunning = false;
     }
 
     // Eventos
     // Añadimos los evenntos necesarios para cada boton. Mediante addEventListener.
 
+    $botonRetroceder.addEventListener('click', function () {
+        retrocederFoto()
+    });
+    $botonAvanzar.addEventListener('click', function () {
+        pasarFoto()
+    });
+    $botonPlay.addEventListener('click', function () {
+        playIntervalo()
+    });
+    $botonStop.addEventListener('click', function () {
+        stopIntervalo()
+    });
+
+    //Detecció de tecles
+    function checkKey(e) {
+        e = e || window.event;
+
+        if (e.keyCode == '37') {
+            retrocederFoto()
+        } else if (e.keyCode == '39') {
+            pasarFoto();
+        } else if (e.keyCode == '32') {
+            if (playrunning) {
+                stopIntervalo();
+                playrunning = false;
+            } else {
+                playIntervalo()
+                playrunning = true;
+            }
+        }
+    }
+
+    //Div segueix el ratolí
+    let circle = document.getElementById('circle');
+    const onMouseMove = (e) => {
+        circle.style.left = e.pageX + 'px';
+        circle.style.top = e.pageY + 'px';
+    }
+    document.addEventListener('mousemove', onMouseMove);
+
     // Iniciar
     renderizarImagen();
-} 
+    document.onkeydown = checkKey;
+
+
+}
